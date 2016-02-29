ee230e2aa2ca6fbec4ece5f015c2ef93a73861cd
Joseph <xiongjiyuan@hotmail.com>
2016-03-01 00:12:26 +0800

    开始完善1

--- src/joseph/drawpad/model/LinearCurve.java
+++ src/joseph/drawpad/model/LinearCurve.java
@@ -61,51 +61,6 @@ public class LinearCurve extends Calculatable {
         return li.inflate(R.layout.editlinear, null);
     }
 
-    /*@Override
-    public LinearCurve scale(float times, float height, float width) {
-        int[] area = new int[2];
-        int Iwidth = Math.round(width);
-        Point[] newPoints = new Point[Iwidth];
-
-        if(times > 1) {
-            int Itimes = Math.round(times);
-            Itimes = Itimes*2;
-            area[0] = Math.round(width/2 - width/Itimes);
-            area[1] = Math.round(width/2 + width/Itimes);
-            int start = area[0];
-            int end = area[1];
-
-            int IntTimes = Math.round(times);
-            for(int i = 0; i < newPoints.length; i = i + IntTimes) {
-                for(int j = 0; j < IntTimes; j++) {
-                    newPoints[i+j] = new Point();
-                    newPoints[i+j].setY(i+j);
-
-                    if(j%IntTimes == 0){
-                        newPoints[i].setX(points[start+i/IntTimes].getyInCurve()*times+height/2f);
-                        newPoints[i].setyInCurve(points[start+i/IntTimes].getyInCurve());
-                        newPoints[i].setxInCurve(points[start+i/IntTimes].getxInCurve());
-                    } else {
-                        float Y,B,A,X;
-                        float y = points[start+i/IntTimes].getY()+j/times;
-                        //Y = (-(width / 2f - y));
-                        y = points[start+i/IntTimes].getxInCurve()+j/times*(points[start+i/IntTimes+1].getxInCurve()-points[start+i/IntTimes].getxInCurve());
-
-                        Y = y;
-                        B = this.getParameters().get(1) ;
-                        A = this.getParameters().get(0);
-                        X = (A * Y + B)*times + height/2;
-                        newPoints[i+j].setyInCurve((X-height/2)/times);
-                        newPoints[i+j].setxInCurve(y);
-                        newPoints[i+j].setX(X);
-                    }
-                }
-            }
-        }
-        setPoints(newPoints);
-        return this;
-    }*/
-
     public Point[] calculate(int width) {
         if(parameters == null || parameters.size() != 2)
             return null;
